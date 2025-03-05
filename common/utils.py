7cc673d6f83041c4cb6919203dfd5028f5153700
pkhudov <pkhudov@gmail.com>
2025-03-05 15:17:30 +0000

    Bring back Dataset to utils

--- common/utils.py
+++ common/utils.py
@@ -14,6 +14,115 @@ from torch_geometric.utils import coalesce, to_undirected
 import random
 import networkx
 
+class HDF5Dataset(Dataset):
+    """Load samples of an PDE Dataset, get items according to PDE"""
+
+    def __init__(self,
+                 path: str,
+                 pde: PDE,
+                 mode: str,
+                 base_resolution: list=None,
+                 super_resolution: list=None,
+                 load_all: bool=False) -> None:
+        """Initialize the dataset object
+        Args:
+            path: path to dataset
+            pde: string of PDE ('CE' or 'WE')
+            mode: [train, valid, test]
+            base_resolution: base resolution of the dataset [nt, nx]
+            super_resolution: super resolution of the dataset [nt, nx]
+            load_all: load all the data into memory
+        Returns:
+            None
+        """
+        super().__init__()
+        f = h5py.File(path, 'r')
+        self.mode = mode
+        self.pde = pde
+        self.dtype = torch.float64
+        self.data = f[self.mode]
+        self.base_resolution = (250, 100) if base_resolution is None else base_resolution
+        self.super_resolution = (250, 200) if super_resolution is None else super_resolution
+        self.dataset_base = f'pde_{self.base_resolution[0]}-{self.base_resolution[1]}'
+        self.dataset_super = f'pde_{self.super_resolution[0]}-{self.super_resolution[1]}'
+
+        ratio_nt = self.data[self.dataset_super].shape[1] / self.data[self.dataset_base].shape[1]
+        ratio_nx = self.data[self.dataset_super].shape[2] / self.data[self.dataset_base].shape[2]
+        assert (ratio_nt.is_integer())
+        assert (ratio_nx.is_integer())
+        self.ratio_nt = int(ratio_nt)
+        self.ratio_nx = int(ratio_nx)
+
+        self.nt = self.data[self.dataset_base].attrs['nt']
+        self.dt = self.data[self.dataset_base].attrs['dt']
+        self.dx = self.data[self.dataset_base].attrs['dx']
+        self.x = self.data[self.dataset_base].attrs['x']
+        self.tmin = self.data[self.dataset_base].attrs['tmin']
+        self.tmax = self.data[self.dataset_base].attrs['tmax']
+
+        if load_all:
+            data = {self.dataset_super: self.data[self.dataset_super][:]}
+            f.close()
+            self.data = data
+
+
+    def __len__(self):
+        return self.data[self.dataset_super].shape[0]
+
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor, list]:
+        """
+        Get data item
+        Args:
+            idx (int): data index
+        Returns:
+            torch.Tensor: numerical baseline trajectory
+            torch.Tensor: downprojected high-resolution trajectory (used for training)
+            torch.Tensor: spatial coordinates
+            list: equation specific parameters
+        """
+        if(f'{self.pde}' == 'CE'):
+            # Super resolution trajectories are downprojected via kernel which averages of neighboring cell values
+            u_super = self.data[self.dataset_super][idx][::self.ratio_nt][None, None, ...]
+            left = u_super[..., -3:-1]
+            right = u_super[..., 1:3]
+            u_super_padded = torch.tensor(np.concatenate((left, u_super, right), -1))
+            weights = torch.tensor([[[[0.2]*5]]])
+            u_super = F.conv2d(u_super_padded, weights, stride=(1, self.ratio_nx)).squeeze().numpy()
+            x = self.x
+
+            # Base resolution trajectories (numerical baseline) and equation specific parameters
+            u_base = self.data[self.dataset_base][idx]
+            variables = {}
+            variables['alpha'] = self.data['alpha'][idx]
+            variables['beta'] = self.data['beta'][idx]
+            variables['gamma'] = self.data['gamma'][idx]
+
+            return u_base, u_super, x, variables
+
+        elif(f'{self.pde}' == 'WE'):
+            # Super resolution trajectories are downprojected via kernel which averages of neighboring cell values
+            # No padding is possible due to non-periodic boundary conditions
+            weights = torch.tensor([[[[1./self.ratio_nx]*self.ratio_nx]]])
+            u_super = self.data[self.dataset_super][idx][::self.ratio_nt][None, None, ...]
+            u_super = F.conv2d(torch.tensor(u_super), weights, stride=(1, self.ratio_nx)).squeeze().numpy()
+
+            # To match the downprojected trajectories, also coordinates need to be downprojected
+            x_super = torch.tensor(self.data[self.dataset_super].attrs['x'][None, None, None, :])
+            x = F.conv2d(x_super, weights, stride=(1, self.ratio_nx)).squeeze().numpy()
+
+            # Base resolution trajectories (numerical baseline) and equation specific parameters
+            u_base = self.data[self.dataset_base][idx]
+            variables = {}
+            variables['bc_left'] = self.data['bc_left'][idx]
+            variables['bc_right'] = self.data['bc_right'][idx]
+            variables['c'] = self.data['c'][idx]
+
+            return u_base, u_super, x, variables
+
+        else:
+            raise Exception("Wrong experiment")
+        
+        
 class GraphCreator(nn.Module):
     def __init__(self,
                  pde: PDE,
